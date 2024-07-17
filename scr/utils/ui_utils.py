cd82c417fff8360a7697db16298faebe8139fc18
zetorik <113109660+zetorik@users.noreply.github.com>
2024-07-17 12:51:22 +0300

    Add files via upload

--- scr/utils/ui_utils.py
+++ scr/utils/ui_utils.py
@@ -1,6 +1,25 @@
 from PySide6.QtWidgets import QVBoxLayout,QLayout
+from PySide6.QtCore import Qt
+from PySide6.QtGui import QPalette,QColor
 from random import randint
+
 from .constants import COLORS
+
+dark_palette = QPalette()
+
+dark_palette.setColor(QPalette.Window, QColor(37, 37, 38))
+dark_palette.setColor(QPalette.WindowText, Qt.white)
+dark_palette.setColor(QPalette.Base, QColor(30, 30, 30))
+dark_palette.setColor(QPalette.AlternateBase, QColor(37, 37, 38))
+dark_palette.setColor(QPalette.ToolTipBase, Qt.white)
+dark_palette.setColor(QPalette.ToolTipText, Qt.white)
+dark_palette.setColor(QPalette.Text, Qt.white)
+dark_palette.setColor(QPalette.Button, QColor(45, 45, 48))
+dark_palette.setColor(QPalette.ButtonText, Qt.white)
+dark_palette.setColor(QPalette.BrightText, Qt.red)
+dark_palette.setColor(QPalette.Link, QColor(42, 130, 218))
+dark_palette.setColor(QPalette.Highlight, QColor(42, 130, 218))
+dark_palette.setColor(QPalette.HighlightedText, Qt.black)
 
 def set_enabled_layout(layout: QVBoxLayout, enabled: bool):
     for i in range(layout.count()):
